d2c309f376b33c60033fea35683be7d3c4d94b6b
Pieter Vantorre <pietervantorre@gmail.com>
2014-06-23 17:28:23 +0200

    converter now requires you to specify at least the input path. output path will be generated if not given. Symbolic links are converted if given as argument.

--- converter.go
+++ converter.go
@@ -4,26 +4,65 @@ import (
 	"fmt"
 	"flag"
 	"os"
-	"path"
+	"path/filepath"
+	"log"
 )
 
 func main() {
 	var input string
 	var output string
-	flag.StringVar(&input, "input", "", "The input file to convert")
-	flag.StringVar(&output, "output", "", "The converted output file")
+	//process command line arguments
+	ProcessArgs(&input, &output)
+	input, output = ValidatePaths(&input, &output)
+	
+	
+	/*data := make([]byte, 100)
+	count, err := file.Read(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("read %d bytes: %q\n", count, data[:count])*/
+	
+	fmt.Printf("input file: %s\n", input)
+	fmt.Printf("output file: %s\n", output)
+}
+
+func ProcessArgs(input, output *string) {
+	flag.StringVar(input, "input", "", "The input file to convert")
+	flag.StringVar(output, "output", "", "The converted output file")
 	flag.Parse()
-	if input == "" {
+	if *input == "" {
 		println("Please specify an input file by using the -input flag")
 		os.Exit(0)
 	}
+}
 
-	if output == "" {
-		output = path.Join(path.Dir(input), "OnlineCGCode.txt")
-		fmt.Printf("Generating output file at %s\n", output)
+func ValidatePaths(input, output *string) (string, string) {
+	//convert symbolic links to the real path
+	var newInput string
+	var newOutput string
+	var err error
+
+	newInput, err = filepath.EvalSymlinks(*input)
+	if err != nil {
+		log.Fatal(err)
 	}
-	fmt.Printf("input file: %s\n", input)
-	fmt.Printf("output file: %s\n", output)
+	//check if the file exists
+	_, err = os.Open(newInput)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if *output == "" {
+		newOutput = filepath.Join(filepath.Dir(newInput), "OnlineCGCode.txt")
+		fmt.Printf("Generating output file at %s\n", newOutput)
+	} else {
+		newOutput, err =filepath.EvalSymlinks(*output)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	return newInput, newOutput
 }
 func ConvertOfflineToOnline(input, output string) {
 
